acf1cafedc723614dfb573969265167a8e2c31fc
Hidaya21 <alabrihidaya@gmail.com>
2026-06-02 11:49:44 +0400

    completed medium problem

--- ArraysPracticeTaskSheet/Program.cs
+++ ArraysPracticeTaskSheet/Program.cs
@@ -66,12 +66,86 @@ namespace ArraysPracticeTaskSheet
                     }
                     break;
                 case 4:
+                    int[] finishTimes = {33,46,51,36,39,69,44,47};
+                    Console.WriteLine("Original Finish Times:");
+                    foreach (int time in finishTimes)
+                    {
+                        Console.WriteLine(time + " seconds");
+                    } 
+                    Array.Sort(finishTimes);
+                    Console.WriteLine("Finish times after sorted:");
+                    foreach (int time in finishTimes)
+                    {
+                        Console.WriteLine(time + " seconds");
+                    }
+                    Console.WriteLine("Number of participants: " + finishTimes.Length);
                     break;
                 case 5:
+                    int[] grades = {99,96,87,98,100,90,94,89,88,91};
+                    Array.Sort(grades);
+                    Array.Reverse(grades);
+                    Console.WriteLine("ascending order:");
+                    for (int i = 0; i < grades.Length; i++ )
+                    {
+                        Console.WriteLine(grades[i]);
+                    }
+                    
                     break;
                 case 6:
+                    int[] quantities = {8, 7, 15, 12, 21,13,6,19};
+                    int total = 0;
+                    for (int i = 0; i < quantities.Length; i++)
+                    {
+                        total= total + quantities[i];
+                    }
+                    Console.WriteLine("toatal stock: " + total);
+                    double average = total / quantities.Length;
+                    Console.WriteLine("stock per slot: " + average);
+                    int target = 15;
+                    int indexs = Array.IndexOf(quantities, target);
+                    if (indexs != -1)
+                    {
+                        Console.WriteLine("Quantity " + target + " found at index: " + indexs);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Quantity not found .");
+                    }
                     break;
                 case 7:
+                    int[] copies = {0,20,35,30,25,33,15,32,22};
+                    Console.WriteLine("Original order:");
+                    foreach (int copy in copies)
+                    {
+                        Console.WriteLine(copy);
+                    }
+                    Array.Sort(copies);
+                    Console.WriteLine("Sorted order:");
+                    foreach (int copy in copies)
+                    {
+                        Console.WriteLine(copy);
+                    }
+                    int maxCopies = copies[copies.Length - 1];
+                    Console.WriteLine("The title with the most copies: "+ maxCopies);
+                    bool hasZero = false;
+                    for (int i = 0; i < copies.Length; i++)
+                    {
+                        if (copies[i] == 0)
+                        {
+                            hasZero = true;
+                            break;
+                        }
+                        
+                    }
+                    if (hasZero)
+                    {
+                        Console.WriteLine("There is at least one book with zero copies.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No books have zero copies.");
+                    }
+
                     break;
                 case 8:
                     break;
